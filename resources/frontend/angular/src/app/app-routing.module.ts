71b629c11ca7a89df9c8a63875bd4dff8d0567ef
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-30 04:17:51 +0200

    Modified angular.json and package.json to fix wrong routes with angular when on dev mode.
    Created component secure.
    Created module public with component public.
    Created components home and login inside module public.
    
    Added routes on app-root module and public module.

--- resources/frontend/angular/src/app/app-routing.module.ts
+++ resources/frontend/angular/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './public/home/home.component';
+import { LoginComponent } from './public/login/login.component';
+import { PublicComponent } from './public/public.component';
+import { SecureComponent } from './secure/secure.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PublicComponent,
+      children:[
+        {path: '', component: HomeComponent},
+        {path: 'login', component: LoginComponent},
+      ]
+  },
+  {path: 'secure', component: SecureComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
