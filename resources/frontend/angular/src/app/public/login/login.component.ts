bc22f3a9fb43885f4172509a78a135494b18c1a9
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-30 20:49:09 +0200

    Created formGroup for form at login component. Added submit function to make a post request to localhost:8000/oauth/token.

--- resources/frontend/angular/src/app/public/login/login.component.ts
+++ resources/frontend/angular/src/app/public/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+  authenticationURL = 'http://localhost:8000/oauth/token';
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      email:'',
+      password:''
+    });
+  }
+
+  submit(){
+    const formData = this.form.value;
+
+    const data = {
+      username: formData.email,
+      password: formData.password,
+      grant_type: 'password',
+      aclient_id: 2,
+      client_secret: 'KwKJnBFw0Q6luee4egvAc8ogfNDckQfeWfBTRleB',
+      scope: '*'
+    }
+
+    this.http.post<any>(this.authenticationURL, data).subscribe(
+      (response) => console.log(response),
+      (error) => console.error(error)
+    );
   }
 
 }
